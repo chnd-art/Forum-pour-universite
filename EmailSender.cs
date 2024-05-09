18f0ab4b928d347b4b46d5fb14995a8b7c615bb7
hindcherrat <hind.cherrat@um5r.ac.ma>
2024-05-09 22:25:11 +0100

    first commit

---
+++ EmailSender.cs
@@ -0,0 +1,28 @@
+﻿using System.Net.Mail;
+using System.Net;
+
+namespace Forum
+{
+    public class EmailSender : IEmailSender
+    {
+        public Task SendEmailAsync(string email, string subject, string message)
+        {
+            var mail = "aspnet423@gmail.com";
+            var pw = "tyndxqgdxhmukruo";
+
+            var client = new SmtpClient("smtp.gmail.com", 587)
+            {
+                EnableSsl = true,
+                UseDefaultCredentials = false,
+                Credentials = new NetworkCredential(mail, pw)
+            };
+
+            return client.SendMailAsync(
+                new MailMessage(from: "aspnet423@gmail.com",
+                                to: email,
+                                subject,
+                                message
+                                ));
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,52 @@
+using Forum;
+using Forum.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+
+// Configure la base de donn�es SQL Server
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(connectionString));
+
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
+builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = false)
+    .AddRoles<IdentityRole>()
+    .AddEntityFrameworkStores<ApplicationDbContext>();
+
+
+
+// Add services to the container.
+
+builder.Services.AddControllersWithViews();
+builder.Services.AddTransient<IEmailSender, EmailSender>();
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+app.UseAuthentication();;
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Questions}/{action=Index}/{id?}");
+
+app.MapRazorPages();
+
+app.Run();
